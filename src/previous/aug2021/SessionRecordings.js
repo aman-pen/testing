73b2c20709a9a7df4f82e36e5c22e97ffeae0494
aman-pen <aman.sharma@penthara.com>
2022-08-18 20:57:37 +0530

    imports in SessionRecordings

--- src/previous/aug2021/SessionRecordings.js
+++ src/previous/aug2021/SessionRecordings.js
@@ -1,5 +1,5 @@
+import React from "react";
 import "./css/SessionRecordings.scss";
-import HeaderData from "./content/HeaderData.json";
 const SessionRecordings = () => {
   return (
     <div className="d-flex flex-column align-items-center aug2021-session-recording">
