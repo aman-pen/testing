de115276d7c54352dedb4c6683039568d0ecf556
aman-pen <aman.sharma@penthara.com>
2022-08-24 01:55:41 +0530

    ModalSponser Compnent addition & Styling

--- src/Header.js
+++ src/Header.js
@@ -13,7 +13,9 @@ import { Navbar, Nav, NavItem, NavLink, Row, Col } from "reactstrap";
 import { isMobile } from "react-device-detect";
 import { slide as Menu } from "react-burger-menu";
 import { ComingSoon } from "./ComingSoon";
+
 import hamburgerclose from "./images/website/close.png";
+import { ModalSponsor } from "./ModalSponsor";
 
 export const Header = () => {
   const [HamburgerOpen, setHamburgerOpen] = useState({ menuOpen: false });
@@ -26,152 +28,162 @@ export const Header = () => {
   const toggleHamburgerMenu = () => {
     setHamburgerOpen({ menuOpen: !HamburgerOpen.menuOpen });
   };
-  return (
-    // <div className="oct2022-header" id="header">
-    <div className="oct2022-header" id="header">
-      <Row className="oct2022-head-nav">
-        <Col xs={3}>
-          <img src={logo} alt="ICSS-logo" className="oct2022-nav-bar-logo" />
-        </Col>
-        {isMobile ? (
-          <>
-            <Col xs={6}> </Col>
-            <Col>
-              <Menu
-                isOpen={HamburgerOpen.menuOpen}
-                onStateChange={(state) => handleHambugerMenuStateChange(state)}
-                right
-                width={"100%"}
-                className={"oct2022-sidebar"}
-                customCrossIcon={
-                  <img
-                    src={hamburgerclose}
-                    onClick={toggleHamburgerMenu}
-                    alt="hamburger-menu"
-                  />
-                }
-              >
-                {NavbarData.map((data) => {
-                  return (
-                    <a
-                      className="menu-item"
-                      href={data.url}
-                      onClick={() => closeHamburgerMenu()}
-                    >
-                      <span>{data.title}</span>
-                    </a>
-                  );
-                })}
-              </Menu>
-            </Col>
-          </>
-        ) : (
-          <Col xs={9} className="oct2022-nav-item">
-            <Navbar>
-              <Nav navbar>
-                {NavbarData.map((data) => {
-                  return (
-                    <NavItem>
-                      <NavLink href={encodeURI(data.url)}>{data.title}</NavLink>
-                    </NavItem>
-                  );
-                })}
-              </Nav>
-            </Navbar>
-          </Col>
-        )}
-      </Row>
-      <Row className="oct2022-head-data">
-        {HeaderData.map((data) => {
-          return (
-            <>
-              <Col md={12} xl={6} className="oct2022-content-left row">
-                <Row className="oct2022-siteTitle">
-                  <Col>
-                    <span>{data.presentedUserGroup}</span>
-                    <h1>{data.siteTitle}</h1>
-                  </Col>
-                </Row>
-                <Row className="oct2022-subTitle">
-                  <Col>
-                    <span>{data.subTitle}</span>
-                  </Col>
-                </Row>
-                <Row className="oct2022-registrationBtn">
-                  <Col>
-                    <a
-                      href={data.registrationLink}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <button
-                        className="oct2022-register-btn"
-                        formTarget="_blank"
-                      >
-                        <span>{data.registrationBtn}</span>
-                      </button>
-                    </a>
-                    {/* ------------------------------------------ */}
+  const [modalSponsor, setModalSponsor] = useState(false);
 
-                    {/* <button
+  const toggle = (data) => {
+    setModalSponsor(!modalSponsor);
+  };
+
+  return (
+    <>
+      <div className="oct2022-header" id="header">
+        <Row className="oct2022-head-nav">
+          <Col xs={3}>
+            <img src={logo} alt="ICSS-logo" className="oct2022-nav-bar-logo" />
+          </Col>
+          {isMobile ? (
+            <>
+              <Col xs={6}> </Col>
+              <Col>
+                <Menu
+                  isOpen={HamburgerOpen.menuOpen}
+                  onStateChange={(state) =>
+                    handleHambugerMenuStateChange(state)
+                  }
+                  right
+                  width={"100%"}
+                  className={"oct2022-sidebar"}
+                  customCrossIcon={
+                    <img
+                      src={hamburgerclose}
+                      onClick={toggleHamburgerMenu}
+                      alt="hamburger-menu"
+                    />
+                  }
+                >
+                  {NavbarData.map((data) => {
+                    return (
+                      <a
+                        className="menu-item"
+                        href={data.url}
+                        onClick={() => closeHamburgerMenu()}
+                      >
+                        <span>{data.title}</span>
+                      </a>
+                    );
+                  })}
+                </Menu>
+              </Col>
+            </>
+          ) : (
+            <Col xs={9} className="oct2022-nav-item">
+              <Navbar>
+                <Nav navbar>
+                  {NavbarData.map((data) => {
+                    return (
+                      <NavItem>
+                        <NavLink href={encodeURI(data.url)}>
+                          {data.title}
+                        </NavLink>
+                      </NavItem>
+                    );
+                  })}
+                </Nav>
+              </Navbar>
+            </Col>
+          )}
+        </Row>
+        <Row className="oct2022-head-data">
+          {HeaderData.map((data) => {
+            return (
+              <>
+                <Col md={12} xl={6} className="oct2022-content-left row">
+                  <Row className="oct2022-siteTitle">
+                    <Col>
+                      <span>{data.presentedUserGroup}</span>
+                      <h1>{data.siteTitle}</h1>
+                    </Col>
+                  </Row>
+                  <Row className="oct2022-subTitle">
+                    <Col>
+                      <span>{data.subTitle}</span>
+                    </Col>
+                  </Row>
+                  <Row className="oct2022-registrationBtn">
+                    <Col>
+                      <a
+                        href={data.registrationLink}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <button
+                          className="oct2022-register-btn"
+                          formTarget="_blank"
+                        >
+                          <span>{data.registrationBtn}</span>
+                        </button>
+                      </a>
+                      {/* ------------------------------------------ */}
+
+                      {/* <button
                       className="oct2022-register-btn"
                       formTarget="_blank"
                     >
                       <span>COMING SOON</span>
                     </button> */}
-                  </Col>
-                </Row>
-              </Col>
-              <Col md={12} xl={6} className="row oct2022-content-right">
-                <Row className="oct2022-handles col-7">
-                  <Col>
-                    <a
-                      href={
-                        "https://twitter.com/search?q=" +
-                        encodeURIComponent(data.twitterHashTag)
-                      }
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <img alt="Twitter logo" src={twitterLogo} />
-                      <span className="oct2022-twitterHashTag">
-                        {data.twitterHashTag}
+                    </Col>
+                  </Row>
+                </Col>
+                <Col md={12} xl={6} className="row oct2022-content-right">
+                  <Row className="oct2022-handles col-7">
+                    <Col>
+                      <a
+                        href={
+                          "https://twitter.com/search?q=" +
+                          encodeURIComponent(data.twitterHashTag)
+                        }
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <img alt="Twitter logo" src={twitterLogo} />
+                        <span className="oct2022-twitterHashTag">
+                          {data.twitterHashTag}
+                        </span>
+                      </a>
+                    </Col>
+                    <Col>
+                      <a
+                        href={"https://twitter.com/" + data.twitterHandle}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <img alt="Twitter logo" src={twitterLogo} />
+                        <span className="oct2022-twitterHandle">
+                          {data.twitterHandle}
+                        </span>
+                      </a>
+                    </Col>
+                  </Row>
+                  <Row className="oct2022-eventDate">
+                    <Col md={5} className="oct2022-eventData-border">
+                      <span>{data.eventDate}</span>
+                    </Col>
+                  </Row>
+                  <Row className="oct2022-fields">
+                    <Col>
+                      <span className="oct2022-eventTitle">
+                        {data.eventTitle}
                       </span>
-                    </a>
-                  </Col>
-                  <Col>
-                    <a
-                      href={"https://twitter.com/" + data.twitterHandle}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <img alt="Twitter logo" src={twitterLogo} />
-                      <span className="oct2022-twitterHandle">
-                        {data.twitterHandle}
-                      </span>
-                    </a>
-                  </Col>
-                </Row>
-                <Row className="oct2022-eventDate">
-                  <Col md={5} className="oct2022-eventData-border">
-                    <span>{data.eventDate}</span>
-                  </Col>
-                </Row>
-                <Row className="oct2022-fields">
-                  <Col>
-                    <span className="oct2022-eventTitle">
-                      {data.eventTitle}
-                    </span>
-                    <div
-                      className={
-                        isMobile
-                          ? "oct2022-fields-row"
-                          : "oct2022-fields-row row"
-                      }
-                    >
-                      <ComingSoon />
+                      <div
+                        className={
+                          isMobile
+                            ? "oct2022-fields-row"
+                            : "oct2022-fields-row row"
+                        }
+                      >
+                        <ComingSoon />
 
-                      {/* <Col className="oct2022-sessionNumber">
+                        {/* <Col className="oct2022-sessionNumber">
                         <span>{Speakers.length}</span>
                         <span>SPEAKERS</span>
                       </Col>
@@ -179,18 +191,34 @@ export const Header = () => {
                         <span>{Sessions.length}</span>
                         <span>SESSIONS</span>
                       </Col>
-                      <Col className="oct2022-sponserNumber">
+                      <Col className="oct2022-SponsorNumber">
                         <span>{Sponsors.length}</span>
                         <span>SPONSORS</span>
                       </Col> */}
-                    </div>
-                  </Col>
-                </Row>
-              </Col>
-            </>
-          );
-        })}
-      </Row>
-    </div>
+                      </div>
+                    </Col>
+                  </Row>
+                </Col>
+              </>
+            );
+          })}
+        </Row>
+        <Row>
+          <div className="oct2022-Be-Sponsor">
+            <div className="oct2022-Be-Sponsor-wrapper">
+              <div className="oct2022-Be-Sponsor-text">
+                Sponsorships for India Cloud Security Summit 2022 are Now Open!
+              </div>
+              <div className="oct2022-Be-Sponsor-btn-wrapper">
+                <button className="oct2022-Be-Sponsor-btn" onClick={toggle}>
+                  <span>Become A Sponsor</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </Row>
+      </div>
+      <ModalSponsor modal={modalSponsor} toggle={toggle} />
+    </>
   );
 };
---
+++ src/ModalSponsor.js
@@ -0,0 +1,213 @@
+import React from "react";
+import "./css/Modal.scss";
+import { Modal, ModalHeader } from "reactstrap";
+
+export const ModalSponsor = ({ modal, toggle }) => {
+  const closeBtn = <button className="oct2022-close" onClick={toggle}></button>;
+
+  return (
+    <>
+      <Modal isOpen={modal} toggle={toggle} className="oct2022-modalSponsor">
+        <div className="oct2022-modalHeader">
+          <ModalHeader
+            toggle={toggle}
+            close={closeBtn}
+            className="oct2022-modal-header"
+          >
+            <div className="oct2022-modal-Headtitle">
+              SPONSORSHIP OPPORTUNITY FOR INDIA CLOUD SECURITY SUMMIT 2022
+            </div>
+          </ModalHeader>
+        </div>
+        <div className="oct2022-modalSponsor-body">
+          <div className="oct2022-modalSponsor-header">
+            <div className="oct2022-modalSponsor-title">BECOME OUR SPONSOR</div>
+            <div className="oct2022-modalSponsor-subtitle">
+              The perfect event to be in touch with Cloud Security professionals
+              and to take the pulse of the latest innovations and trends in the
+              world of Cloud Security.
+            </div>
+          </div>
+          <div className="oct2022-modalSponsor-whyUs">
+            <div className="oct2022-modalSponsor-title oct2022-modalSponsor-whyUs-title">
+              <div className="pt-5">WHY SPONSOR US</div>
+            </div>
+            <div class="row p-4 gx-5 gy-2">
+              <div class="col-12 col-md-6">
+                <div className="oct-2022-grid-img1" />
+                <div className="oct-2022-grid-heading">
+                  Brand &#38; Product Positioning
+                </div>
+                <ul className="oct-2022-grid-subHeading">
+                  <li>
+                    Align yourselves with leading brands and fuel your marketing
+                    engine through tech event sponsorship.
+                  </li>
+                  <li>
+                    Present your products &#38; technologies to a global
+                    audience of IT Pros and developers.
+                  </li>
+                </ul>
+              </div>
+              <div class="col-12 col-md-6">
+                <div className="oct-2022-grid-img2" />
+                <div className="oct-2022-grid-heading">
+                  Talent Attraction &#38; Retention
+                </div>
+                <ul className="oct-2022-grid-subHeading">
+                  <li>
+                    Gain access to a global pool of candidates from different
+                    countries.
+                  </li>
+                  <li>
+                    Simplify the hiring process by building credibility within
+                    the Open-Source community.
+                  </li>
+                  <li>
+                    Communicate to your employees that you are at the forefront
+                    of technology and care about their professional development.
+                  </li>
+                </ul>
+              </div>
+              <div class="w-100"></div>
+              <div class="col-12 col-md-6">
+                <div className="oct-2022-grid-img3" />
+                <div className="oct-2022-grid-heading">Employer Branding</div>
+                <ul className="oct-2022-grid-subHeading">
+                  <li>
+                    Put your company name in line with top industry leaders.
+                  </li>
+                  <li>
+                    Improve company image and brand exposure globally across a
+                    significant number of targeted developer groups and
+                    communities.
+                  </li>
+                </ul>
+              </div>
+              <div class="col-12 col-md-6">
+                <div className="oct-2022-grid-img4" />
+                <div className="oct-2022-grid-heading">Lead Generation</div>
+                <ul className="oct-2022-grid-subHeading">
+                  <li>
+                    Increase brand awareness and drive sales pipeline growth.
+                  </li>
+                  <li>
+                    Community events are not typically associated with high lead
+                    generation; however, it is one of the best places to promote
+                    your products and services and make new prospect leads.
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="oct2022-modalSponsor-schemes">
+            <div className="oct2022-modalSponsor-title oct2022-modalSponsor-schemes-title">
+              <div className="pt-5">SPONSORSHIP SCHEMES</div>
+            </div>
+            <div class="row p-4 align-items-start gx-4">
+              <div class="col-12 col-md-4 ">
+                <div className="scheme-card">
+                  <div className="oct2022-modalSponsor-schemes-header1">
+                    <div>Gold Sponsor</div>
+                  </div>
+                  <div className="d-flex flex-column align-items-center oct2022-modalSponsor-schemes-body">
+                    <div className="d-flex flex-column align-items-center justify-content-center">
+                      <span className="oct2022-moneyText1">20,000 INR</span>
+                      <div className="oct2022-moneyText1-para">
+                        For small to medium sized organizations
+                      </div>
+                    </div>
+                    <div className="oct2022-Sponsor-ulDiv">
+                      <ul className="d-flex flex-column align-items-start">
+                        <div className="oct2022-Sponsor-divider" />
+                        <li>Company logos on the event website as sponsors.</li>
+                        <div className="oct2022-Sponsor-divider" />
+                        <li>
+                          Company logos on the event banners, session slides,
+                          swags, and giveaways.
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-12 col-md-4">
+                <div className="scheme-card">
+                  <div className="oct2022-modalSponsor-schemes-header2">
+                    <div>Platinum Sponsor</div>
+                  </div>
+                  <div className="d-flex flex-column align-items-center oct2022-modalSponsor-schemes-body">
+                    <div className="d-flex flex-column align-items-center justify-content-center">
+                      <span className="oct2022-moneyText2">25,000 INR</span>
+                      <div className="oct2022-moneyText2-para">
+                        For small to medium sized organizations
+                      </div>
+                    </div>
+                    <div className="oct2022-Sponsor-ulDiv">
+                      <ul className="d-flex flex-column align-items-start">
+                        <div className="oct2022-Sponsor-divider" />
+                        <li>Company logos on the event website as sponsors.</li>
+                        <div className="oct2022-Sponsor-divider" />
+                        <li>
+                          Company logos on the event banners, session slides,
+                          swags, and giveaways.
+                        </li>
+                        <div className="oct2022-Sponsor-divider" />
+                        <li>
+                          5% off India Cloud Security Summit 2023 Sponsorship
+                          Fee.
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-12 col-md-4 ">
+                <div className="scheme-card">
+                  <div className="oct2022-modalSponsor-schemes-header3">
+                    <div>Diamond Sponsor</div>
+                  </div>
+                  <div className="d-flex flex-column align-items-center oct2022-modalSponsor-schemes-body">
+                    <div className="d-flex flex-column align-items-center justify-content-center">
+                      <span className="oct2022-moneyText3">30,000 INR</span>
+                      <div className="oct2022-moneyText3-para">
+                        For small to medium sized organizations
+                      </div>
+                    </div>
+                    <div className="oct2022-Sponsor-ulDiv">
+                      <ul className="d-flex flex-column align-items-start">
+                        <div className="oct2022-Sponsor-divider" />
+                        <li>All the Benefits in Platinum Sponsorship.</li>
+                        <div className="oct2022-Sponsor-divider" />
+                        <li>Attendee opt. in contact details.</li>
+                        <div className="oct2022-Sponsor-divider" />
+                        <li>Company Logo in the pre-event mail.</li>
+                        <div className="oct2022-Sponsor-divider" />
+                        <li>
+                          A slot to present or talk about your product or any
+                          Microsoft Cloud Security product that you recently
+                          worked on.
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="oct2022-btn-wrapper d-flex align-items-center justify-content-center">
+                <a
+                  className="oct2022-sponsorButton"
+                  href="https://forms.office.com/pages/responsepage.aspx?id=AFFBWLwhD0qOAc-aLhfjZU0ZvfSemStNkEqZOX_VTVFUQ0FOR0daTEw3SVQ1S0JWTVA3SFE2MjJNVS4u"
+                >
+                  <span>Become A Sponsor now</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal>
+    </>
+  );
+};
